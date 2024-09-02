63b9fdbd4814211ed461674a6c2fa447edf47990
Asbelit0 <djasbel10@gmail.com>
2024-09-01 21:46:34 -0500

    Evneto_boton_buscar

--- ip-information.py
+++ ip-information.py
@@ -3,32 +3,41 @@ from tkinter import ttk
 import requests
 
 class Aplicacion:
+    COLOR_FONDO = "grey"
+    FUENTE = ("Arial", 12, "italic", "bold")
+
     def __init__(self, raiz):
         self.raiz = raiz
         self.raiz.title("Buscar información de una dirección IP")
         self.raiz.geometry("550x300")
-        self.raiz.configure(bg="grey")
+        self.raiz.configure(bg=self.COLOR_FONDO)
 
-        # Crear etiquetas y campos de texto
-        self.etiqueta_direccion_ip = ttk.Label(self.raiz, text="Dirección IP:", background="Grey", 
-                                                font=("Arial", 12, "italic", "bold"))
-        self.etiqueta_direccion_ip.grid(column=0, row=0, padx=5, pady=20)
+        self.crear_widgets()
 
-        self.campo_direccion_ip = ttk.Entry(self.raiz, width=30, background="Grey", 
-                                            font=("Arial", 11, "italic", "bold"))
+    def crear_widgets(self):
+        #Etiquetas y campos de texto
+        self.label_ip_address = ttk.Label(self.raiz, text="Dirección IP:", background=self.COLOR_FONDO, 
+                                        font=self.FUENTE)
+        self.label_ip_address.grid(column=0, row=0, padx=5, pady=20)
+
+        self.campo_direccion_ip = ttk.Entry(self.raiz, width=30, background=self.COLOR_FONDO, 
+                                            font=self.FUENTE)
         self.campo_direccion_ip.grid(column=1, row=0, padx=5, pady=5)
         self.campo_direccion_ip.config(justify="center")
 
-        self.etiqueta_resultado = ttk.Label(self.raiz, text="Resultado:", background="Grey", 
-                                            font=("Arial", 12, "italic", "bold"))
-        self.etiqueta_resultado.grid(column=0, row=1, padx=5, pady=5)
+        self.label_resultado = ttk.Label(self.raiz, text="Resultado:", background=self.COLOR_FONDO, 
+                                        font=self.FUENTE)
+        self.label_resultado.grid(column=0, row=1, padx=5, pady=5)
 
-        self.campo_resultado = tk.Text(self.raiz, width=50, height=10)
+        self.campo_resultado = tk.Text(self.raiz, width=50, height=10, borderwidth=2, relief="groove")
         self.campo_resultado.grid(column=1, row=1, padx=5, pady=5)
 
-        # Crear botón para buscar información
+        #Botón para buscar información
         self.boton_buscar = ttk.Button(self.raiz, text="Buscar", command=self.buscar_informacion)
         self.boton_buscar.grid(column=1, row=2, padx=5, pady=25)
+
+        #Evento boton buscar
+        self.campo_direccion_ip.bind("<Return>", lambda event: self.buscar_informacion())
 
     def buscar_informacion(self):
         direccion_ip = self.campo_direccion_ip.get()
@@ -37,19 +46,24 @@ class Aplicacion:
             respuesta = requests.get(url)
             if respuesta.status_code == 200:
                 datos = respuesta.json()
-                self.campo_resultado.delete(1.0, tk.END)
-                self.campo_resultado.insert(tk.END, f"País: {datos['country']}\n")
-                self.campo_resultado.insert(tk.END, f"Región: {datos['region']}\n")
-                self.campo_resultado.insert(tk.END, f"Ciudad: {datos['city']}\n")
-                self.campo_resultado.insert(tk.END, f"Coordenadas: {datos['lat']}, {datos['lon']}\n")
-                self.campo_resultado.insert(tk.END, f"Organización: {datos['org']}\n")
-                self.campo_resultado.insert(tk.END, f"Proveedor: {datos['isp']}\n")
+                self.mostrar_resultado(datos)
             else:
-                self.campo_resultado.delete(1.0, tk.END)
-                self.campo_resultado.insert(tk.END, "Error al buscar información")
+                self.mostrar_error("Error al buscar información")
         else:
-            self.campo_resultado.delete(1.0, tk.END)
-            self.campo_resultado.insert(tk.END, "Por favor, ingrese una dirección IP")
+            self.mostrar_error("Por favor, ingrese una dirección IP")
+
+    def mostrar_resultado(self, datos):
+        self.campo_resultado.delete(1.0, tk.END)
+        self.campo_resultado.insert(tk.END, f"País: {datos['country']}\n")
+        self.campo_resultado.insert(tk.END, f"Región: {datos['region']}\n")
+        self.campo_resultado.insert(tk.END, f"Ciudad: {datos['city']}\n")
+        self.campo_resultado.insert(tk.END, f"Coordenadas: {datos['lat']}, {datos['lon']}\n")
+        self.campo_resultado.insert(tk.END, f"Organización: {datos['org']}\n")
+        self.campo_resultado.insert(tk.END, f"Proveedor: {datos['isp']}\n")
+
+    def mostrar_error(self, mensaje):
+        self.campo_resultado.delete(1.0, tk.END)
+        self.campo_resultado.insert(tk.END, mensaje)
 
 if __name__ == "__main__":
     raiz = tk.Tk()
